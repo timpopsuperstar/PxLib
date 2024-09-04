ee53a90ed93dc5338a10caaa23c93cfa9552c5ac
timpopsuperstar <timpopsuperstar@gmail.com>
2024-09-04 13:12:51 -0500

    wipe freal

--- Assets/Scripts/Chef/Battle/CombatData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class CombatData
-{
-    public static Vector2 PlayerHomePosition => new Vector2(0, 0);
-    public static Vector2 EnemyHomePosition => new Vector2(0, 0);
-}
